a7aeb308877708701e16b68e0d77cf357805d9a9
EstebanFlorence <77881363+EstebanFlorence@users.noreply.github.com>
2023-12-01 15:45:19 +0100

    CPP02 ex03

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -2,7 +2,8 @@
 
 Fixed	area(const Point& a, const Point& b, const Point& c)
 {
-
+	return 0.5 * std::abs((a.getX() - b.getX()) * (a.getY() - c.getY())
+						- (a.getY() - b.getY()) * (a.getX() - c.getY()));
 }
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
