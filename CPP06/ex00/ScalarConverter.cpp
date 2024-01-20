e82abc051c64448f2fbb4084823a720501902185
EstebanFlorence <77881363+EstebanFlorence@users.noreply.github.com>
2024-01-20 14:20:10 +0100

    CPP06 innit

---
+++ CPP06/ex00/ScalarConverter.cpp
@@ -0,0 +1,12 @@
+#include "ScalarConverter.hpp"
+
+ScalarConerter::ScalarConerter() {}
+
+ScalarConerter::ScalarConerter(const ScalarConerter& copy) {}
+
+ScalarConerter::~ScalarConerter() {}
+
+ScalarConerter&	ScalarConerter::operator=(const ScalarConerter& other)
+{
+	
+}
---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,18 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <iostream>
+
+class	ScalarConerter
+{
+	private:
+	ScalarConerter();
+	ScalarConerter(const ScalarConerter& copy);
+	ScalarConerter&	operator=(const ScalarConerter& other);
+
+	public:
+	~ScalarConerter();
+
+};
+
+#endif
